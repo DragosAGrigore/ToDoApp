3afa73ee2b7074860719e9febd7d2aa1e749bde3
DragosAGrigore <dragos.grigore@mail.com>
2020-10-22 23:48:33 +0300

    Changed Create functionality to update the DOM instead of refreshing the page

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,30 @@
+function itemTemplate(item) {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+        <span class="item-text">${item.text}</span>
+        <div>
+          <button data-id=${item._id} class="update-me btn btn-secondary btn-sm mr-1">Update</button>
+          <button data-id=${item._id} class="delete-me btn btn-danger btn-sm">Delete</button>
+        </div>
+      </li>
+      `
+}
+
+document.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let inputField = document.getElementById("create-field");
+    let inputText = inputField.value;
+    inputField.value = "";
+    inputField.focus();
+    if (inputText) {
+        axios.post("/create-item", { item: inputText }).then(function(response) {
+            let ul = document.getElementById("item-list");
+            ul.insertAdjacentHTML("beforeend", itemTemplate(response.data));
+        }).catch(function(error) {
+            alert("Something went wrong. Try again");
+        })
+    } 
+});
+
 document.addEventListener("click", function(event) {
     if (event.target.classList.contains("update-me")) {
         let itemElement = event.target.parentElement.parentElement.querySelector(".item-text");
