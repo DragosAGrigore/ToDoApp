1aa536a81a580b152228922c3e6a69ae2c7f530c
DragosAGrigore <dragos.grigore@mail.com>
2020-10-23 23:46:52 +0300

    Added security and sanitizer

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 let config = require('./config');
 let mongodb = require('mongodb');
 let express = require('express');
+let sanitizeHTML = require('sanitize-html');
+
 let app = express();
 
 const ITEMS_KEY = "items";
@@ -9,6 +11,17 @@ let db;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(passwordProtected);
+
+function passwordProtected(req, res, next) {
+  res.set("WWW-Authenticate", "Basic realm='ToDo app'");
+  if (req.headers.authorization === "Basic YWRtaW46YWRtaW4=") {
+    next();
+  } else {
+    res.status(401).send("Forbidden.");
+  }
+}
 
 let connectionString = `mongodb+srv://${config.username}:${config.password}@cluster0.ray9g.mongodb.net/ToDoApp`;
 mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
@@ -59,13 +72,17 @@ app.get('/', function(req, res) {
 });
 
 app.post("/create-item", function(req, res) {
-  db.collection(ITEMS_KEY).insertOne({ text: req.body.item }, function(err, doc) {
+  let cleanText = sanitizeHTML(req.body.item);
+  console.log(cleanText);
+  db.collection(ITEMS_KEY).insertOne({ text: cleanText }, function(err, doc) {
     res.json(doc.ops[0]);
   });
 });
 
 app.post("/update-item", function(req, res) {
-  db.collection(ITEMS_KEY).findOneAndUpdate({ _id: new mongodb.ObjectId(req.body.id) }, { $set: { text: req.body.text } }, function(err, doc) {
+  let cleanText = sanitizeHTML(req.body.item);
+  console.log(cleanText);
+  db.collection(ITEMS_KEY).findOneAndUpdate({ _id: new mongodb.ObjectId(req.body.id) }, { $set: { text: cleanText } }, function(err, doc) {
     res.send("Success");
   });
 });
